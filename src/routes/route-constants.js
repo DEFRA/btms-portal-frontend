08ac3e14cc23f93a6190d196babf74c7a83360c7
Matt O'Donoghue <mattyod@gmail.com>
2025-04-28 12:15:03 +0100

    update Entra sign in url

--- src/routes/route-constants.js
+++ src/routes/route-constants.js
@@ -3,7 +3,7 @@ const paths = {
   LANDING: '/',
   AUTH_DEFRA_ID_CALLBACK: '/signin-oidc',
   SIGN_IN: '/sign-in',
-  SIGN_IN_ENTRA: '/sign-in-entra',
+  SIGN_IN_ENTRA: '/signin-entra-id',
   SIGN_OUT: '/sign-out',
   SEARCH: '/search',
   SEARCH_RESULT: '/search-result'
