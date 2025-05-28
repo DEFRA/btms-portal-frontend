bf408fdf6349e1631c221b70d8de0ce0274b00af
Matt O'Donoghue <mattyod@gmail.com>
2025-05-28 15:07:08 +0100

    CDMS-264: result page filters CDMS-398: line item spacing
    * add ability to filter MRN by match, decision & authority
    * add ability to filter CHED by authority
    * maintain filter state in url query string for sharing/refreshing
    * url history is replaced so back button still returns to /search
    * decisions for MRNs now grouped by CHED reference
    * general styling updated to match designs

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -7,6 +7,7 @@ import {
   Radios,
   SkipLink
 } from 'govuk-frontend'
+import { filters } from './filters.js'
 
 createAll(Button)
 createAll(Checkboxes)
@@ -14,3 +15,6 @@ createAll(ErrorSummary)
 createAll(Header)
 createAll(Radios)
 createAll(SkipLink)
+
+window.BTMS = window.BTMS || {}
+window.BTMS.filters = filters
---
+++ src/client/javascripts/filters.js
@@ -0,0 +1,143 @@
+const filterTypes = {
+  declaration: {
+    all: ['decision', 'authority', 'match'],
+    list: ['decision', 'authority'],
+    row: ['match']
+  },
+  notification: {
+    all: ['chedAuthority'],
+    list: ['chedAuthority'],
+    row: []
+  }
+}
+
+const noResultsInset = () => {
+  const div = document.createElement('div')
+  div.classList.add('govuk-inset-text')
+  div.innerText = 'There are no items that match the filters selected.'
+
+  return div
+}
+
+const setState = (event) => {
+  const { name, value } = event.target
+
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  value ? params.set(name, value) : params.delete(name)
+
+  const newUrl = `${url.pathname}?${params.toString()}`
+  window.history.replaceState({}, '', newUrl)
+}
+
+const getState = (filterTypes) => {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  const entries = filterTypes
+    .map((key) => [key, params.get(key)])
+    .filter(([_, value]) => value)
+
+  return Object.fromEntries(entries)
+}
+
+const resetState = (filterTypes) => {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  filterTypes.forEach((name) => params.delete(name))
+
+  const newUrl = `${url.pathname}?${params.toString()}`
+  window.history.replaceState({}, '', newUrl)
+  return {}
+}
+
+const clearButton = (state, id) => {
+  const button = document.getElementById(id)
+  button.hidden = Object.keys(state).length === 0
+}
+
+const selects = (state) => {
+  Object.entries(state).forEach(([key, value]) => {
+    document.getElementById(key).value = value
+  })
+}
+
+const setEmptyState = (table) => {
+  const hasVisibleRows = table
+    .querySelectorAll('tbody > tr:not([hidden])').length > 0
+  const header = table.querySelector('thead')
+
+  if (!hasVisibleRows && header.hidden === false) {
+    header.hidden = true
+    const inset = noResultsInset()
+    table.before(inset)
+  }
+  if (hasVisibleRows && header.hidden === true) {
+    table.parentElement.querySelector('.govuk-inset-text').remove()
+    header.hidden = false
+  }
+}
+
+const fixOutcomeWidths = () => {
+  [...document.querySelectorAll('.btms-decision-outcomes')]
+    .forEach((cell) => { cell.style.width = `${cell.offsetWidth}px` })
+}
+
+const setRows = (state, target) => {
+  const tables = [...document.querySelectorAll(`table.btms-${target}`)]
+
+  tables.forEach((table) => {
+    const rows = [...table.querySelectorAll('tbody tr')]
+
+    rows.forEach((row) => {
+      const decisionsList = row.querySelector('ul')
+      const listItems = [...decisionsList.querySelectorAll('li')]
+
+      listItems.forEach((li) => {
+        li.hidden = filterTypes[target].list.some((key) =>
+          state[key] && li.dataset[key] !== state[key]
+        )
+      })
+
+      const rowHidden = filterTypes[target].row.some((key) =>
+        state[key] && row.dataset[key] !== state[key]
+      )
+
+      row.hidden = Boolean(
+        decisionsList.querySelectorAll('li:not([hidden])').length === 0 ||
+        rowHidden
+      )
+    })
+
+    setEmptyState(table)
+  })
+}
+
+const setUpFilters = (type) => {
+  const filters = document.getElementById(`${type}-filters`)
+  filters.removeAttribute('hidden')
+  filters.addEventListener('change', (event) => {
+    setState(event)
+    const state = getState(filterTypes[type].all)
+    clearButton(state, `${type}-reset`)
+    setRows(state, type)
+  })
+  filters.addEventListener('reset', () => {
+    const state = resetState(filterTypes[type].all)
+    clearButton(state, `${type}-reset`)
+    setRows(state, type)
+  })
+
+  const state = getState(filterTypes[type].all)
+  clearButton(state, `${type}-reset`)
+  selects(state)
+  setRows(state, type)
+}
+
+export const filters = () => {
+  setUpFilters('declaration')
+  setUpFilters('notification')
+  fixOutcomeWidths()
+}
