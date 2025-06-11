2fb88cb5ebb5c848015ee92a110eb87ae6cbc320
Matthew O'Donoghue <mattyod@gmail.com>
2025-06-11 10:26:58 +0100

    CDMS-401: link notifications to IPAFFS (#53)

--- .jest/setup-file.js
+++ .jest/setup-file.js
@@ -5,3 +5,4 @@ global.fetch = fetchMock
 process.env.BTMS_API_USERNAME='usr-name-for-unit-tests'
 process.env.BTMS_API_PASSWORD='pwd-for-unit-tests'
 process.env.BTMS_API_BASE_URL='https://btms-api-base-url-for-unit-tests/api'
+process.env.IPAFFS_URL='https://ipaffs/CHED_REFERENCE/ched'
