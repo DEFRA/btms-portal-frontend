431fa5201b6a05f40a5ec0f8b4005f37d6aa1f18
Matthew O'Donoghue <mattyod@gmail.com>
2025-07-03 16:42:50 +0100

    CDMS-665: account management and sign out links (#88)
    * only show account management if logged in with DefraId
    * signed-out page dynamically links back to Defra or Entra sign in paths
    * hoist header options up into context
    * allow manifest loading to fail and error
    * log if there's ever an attempt to load a file not in the manifest

--- src/routes/signed-out.js
+++ src/routes/signed-out.js
@@ -1,9 +1,19 @@
+import joi from 'joi'
 import { paths } from './route-constants.js'
 
 export const signedOut = {
   method: 'get',
   path: paths.SIGNED_OUT,
-  handler: async (_request, h) => {
-    return h.view('signed-out', { signInUrl: paths.SIGN_IN })
+  options: {
+    validate: {
+      query: joi.object({
+        provider: joi.string()
+      })
+    }
+  },
+  handler: async (request, h) => {
+    const { provider } = request.query
+    const signInUrl = provider === 'entraId' ? paths.SIGN_IN_ENTRA : paths.SIGN_IN
+    return h.view('signed-out', { signInUrl })
   }
 }
