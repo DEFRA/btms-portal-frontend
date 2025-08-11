810d930990597031a22e643c0dbe15a57844d86a
Matt O'Donoghue <mattyod@gmail.com>
2025-08-11 10:37:44 +0100

    CDMS-963: fix search-results cookie banner issue
    Cookie banner was taking over the search results query string so users
    were getting kicked back to the search page with an error for missing
    search query.
    
    Also:
    
    * fix issue where submitting unchecked radio options on the cookies page
    * provide errors on the cookie page when submitting unchecked radios
    * remove duplicate paragraph from the cookies page

--- test/integration/google-analytics.test.js
+++ test/integration/google-analytics.test.js
@@ -1,37 +1,44 @@
+import globalJsdom from 'global-jsdom'
 import { initialiseServer } from '../utils/initialise-server.js'
 import { paths } from '../../src/routes/route-constants.js'
-import globalJsdom from 'global-jsdom'
+import { config } from '../../src/config/config.js'
+
+const gtmId = config.get('gtmId')
 
 test('when analytics accepted, google tag manager is present', async () => {
   const server = await initialiseServer()
 
   const { payload } = await server.inject({
-    headers: {
-      Cookie: 'cookie_policy=' + Buffer.from('{"analytics":true}').toString('base64')
-    },
     method: 'get',
-    url: paths.LANDING
+    url: paths.LANDING,
+    headers: {
+      cookie: 'cookiePolicy=' + Buffer.from('{"analytics":"yes"}').toString('base64')
+    }
   })
 
   globalJsdom(payload)
 
-  const gtmScript = [...document.getElementsByTagName('script')].find(s => s.innerHTML.includes('gtm'))
-  expect(gtmScript).not.toBeUndefined()
+  expect(document.querySelector(`script[data-id='${gtmId}']`))
+    .toBeInTheDocument()
+  expect(document.querySelector(`noscript[data-id='${gtmId}']`))
+    .toBeInTheDocument()
 })
 
 test('when analytics not accepted, google tag manager is not present', async () => {
   const server = await initialiseServer()
 
   const { payload } = await server.inject({
-    headers: {
-      Cookie: 'cookie_policy=' + Buffer.from('{"analytics":false}').toString('base64')
-    },
     method: 'get',
-    url: paths.LANDING
+    url: paths.LANDING,
+    headers: {
+      cookie: 'cookiePolicy=' + Buffer.from('{"analytics":"no"}').toString('base64')
+    }
   })
 
   globalJsdom(payload)
 
-  const gtmScript = [...document.getElementsByTagName('script')].find(s => s.innerHTML.includes('gtm'))
-  expect(gtmScript).toBeUndefined()
+  expect(document.querySelector(`script[data-id='${gtmId}']`))
+    .not.toBeInTheDocument()
+  expect(document.querySelector(`noscript[data-id='${gtmId}']`))
+    .not.toBeInTheDocument()
 })
