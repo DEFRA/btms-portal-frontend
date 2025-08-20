df44a2fe230442485db4eaed6ae73d202d882e0d
Matthew O'Donoghue <mattyod@gmail.com>
2025-08-20 17:14:26 +0100

    CDMS-922: use correct callback url for EntraId (#152)
    Co-authored-by: Mohammed Shahin Uddin <mo@thetestroom.com>

--- src/auth/refesh-token.js
+++ src/auth/refesh-token.js
@@ -1,4 +1,4 @@
-import { config, configKeys } from '../config/config.js'
+import { config } from '../config/config.js'
 import { getUserSession } from './user-session.js'
 import { getOpenIdRefreshToken } from './open-id-client.js'
 import { paths } from '../routes/route-constants.js'
@@ -8,11 +8,14 @@ async function refreshAccessToken (request) {
   request.logger.setBindings({ refreshingAccessToken: authedUser.strategy })
 
   const authConfig = config.get('auth')[authedUser.strategy]
-  const refreshToken = authedUser?.refreshToken ?? null
+  const refreshToken = authedUser.refreshToken
   const clientId = authConfig.clientId
   const clientSecret = authConfig.clientSecret
   const scopes = authConfig.scopes.join(' ')
-  const redirectUri = config.get(configKeys.APP_BASE_URL) + paths.AUTH_DEFRA_ID_CALLBACK
+  const callbackPath = authedUser.strategy === 'defraId'
+    ? paths.SIGNIN_DEFRA_ID_CALLBACK
+    : paths.SIGNIN_ENTRA_ID_CALLBACK
+  const redirectUri = config.get('appBaseUrl') + callbackPath
 
   const params = {
     client_id: clientId,
