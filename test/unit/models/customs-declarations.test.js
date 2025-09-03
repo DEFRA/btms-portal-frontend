d4655b7b2d20824f330e42e25a1a91e01d0b0677
Matt O'Donoghue <mattyod@gmail.com>
2025-09-03 11:06:52 +0100

    CDMS-1045: add E99 no match decision code

--- test/unit/models/customs-declarations.test.js
+++ test/unit/models/customs-declarations.test.js
@@ -922,7 +922,8 @@ test.each([
   { internalDecisionCode: 'E71' },
   { internalDecisionCode: 'E72' },
   { internalDecisionCode: 'E73' },
-  { internalDecisionCode: 'E87' }
+  { internalDecisionCode: 'E87' },
+  { internalDecisionCode: 'E99' }
 ])(
   'the match indicator is set correctly depending on the internal decision code $internalDecisionCode',
   ({ internalDecisionCode }) => {
