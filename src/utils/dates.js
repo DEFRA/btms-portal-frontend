1ec6b1d6d533fed3cbfbbb5b0c6fd96ea90221ba
Matt O'Donoghue <mattyod@gmail.com>
2025-10-03 15:18:10 +0100

    CDMS-904: add charts tab to reporting page

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -26,3 +26,31 @@ export const getFromAndTo = (startDate, endDate) => {
     addMilliseconds(endDate, oneMillisecond).toISOString()
   ]
 }
+
+const idealIntervalCount = 30
+
+const humanFriendlyIntervalDurations = [
+  60000, 120000, 300000, 600000, 1200000, 1800000, 3600000, 7200000, 10800000,
+  14400000, 18000000, 21600000, 25200000, 28800000, 32400000, 36000000,
+  39600000, 43200000, 86400000, 172800000, 345600000
+]
+
+export const getIntervals = (startDate, endDate) => {
+  const intervals = []
+
+  const diffInMS = Math.floor(endDate - startDate)
+  const exactIntervalDuration = Math.floor(diffInMS / idealIntervalCount)
+  const intervalDuration = humanFriendlyIntervalDurations.find(
+    (duration) => duration >= exactIntervalDuration
+  )
+
+  for (let index = 1; index < idealIntervalCount; index += 1) {
+    const interval = new Date(startDate.getTime() + index * intervalDuration)
+
+    if (interval.getTime() <= endDate.getTime() + 1) {
+      intervals.push(interval.toISOString())
+    }
+  }
+
+  return intervals
+}
