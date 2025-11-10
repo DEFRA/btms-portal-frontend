uk-gov-mirror/defra.btms-portal-frontend

CDMS-1216: emit GMR search related metrics

src/models/model-constants.js
@@ -165,3 +165,15 @@ export const ORDERED_CLEARANCE_DECISIONS = [
   { type: 'item', code: 'C07', description: `Release - ${decisionCodeDescriptions.C07}` },
   { type: 'item', code: 'C08', description: `Release - ${decisionCodeDescriptions.C08}` }
 ]
+
+export const metricsNames = {
+  MRN: 'search.mrn',
+  CHED_ID: 'search.chedId',
+  DUCR: 'search.ducr',
+  SIGNIN_ENTRA_ID: 'signIn.entraId',
+  SIGNIN_DEFRA_ID: 'signIn.defraId',
+  GMR_ID: 'search.gmrId',
+  GMR_NOT_FOUND: 'gmr.search.not.found',
+  GMR_KNOWN_MRNS: 'gmr.known.mrns',
+  GMR_UNKNOWN_MRNS: 'gmr.unknown.mrns'
+}

src/routes/gmr-search-result.js
@@ -3,6 +3,8 @@ import { createRouteConfig } from './search-result-common.js'
 import { getRelatedImportDeclarations } from '../services/related-import-declarations.js'
 import { mapGoodsVehicleMovements } from '../models/goods-vehicle-movements.js'
 import { searchKeys } from '../services/search-patterns.js'
+import { metricsCounter } from '../utils/metrics.js'
+import { metricsNames } from '../models/model-constants.js'
 
 const searchTermValidator = (key, pattern, value) => {
   return key === searchKeys.GMR_ID && pattern.test(value)
@@ -18,6 +20,8 @@ export const gmrSearchResult = createRouteConfig(searchTermValidator, paths.GMR_
       isValid: false,
       errorCode: 'SEARCH_TERM_NOT_FOUND'
     })
+
+    metricsCounter(metricsNames.GMR_NOT_FOUND)
 
     return h.redirect(paths.SEARCH).takeover()
   }

src/routes/signin-oidc-entra.js
@@ -1,6 +1,7 @@
 import { paths } from './route-constants.js'
 import { setUserSession } from '../auth/user-session.js'
 import { metricsCounter } from '../utils/metrics.js'
+import { metricsNames } from '../models/model-constants.js'
 
 export const signinOidcEntra = {
   method: ['get', 'post'],
@@ -13,7 +14,7 @@ export const signinOidcEntra = {
     await setUserSession(request, sessionId)
     request.cookieAuth.set({ sessionId })
 
-    metricsCounter('signIn.entraId')
+    metricsCounter(metricsNames.SIGNIN_ENTRA_ID)
     return h.redirect(paths.SEARCH)
   }
 }

src/routes/signin-oidc.js
@@ -1,6 +1,7 @@
 import { paths } from './route-constants.js'
 import { setUserSession } from '../auth/user-session.js'
 import { metricsCounter } from '../utils/metrics.js'
+import { metricsNames } from '../models/model-constants.js'
 
 export const signinOidc = {
   method: ['get', 'post'],
@@ -13,7 +14,7 @@ export const signinOidc = {
     await setUserSession(request, sessionId)
     request.cookieAuth.set({ sessionId })
 
-    metricsCounter('signIn.defraId')
+    metricsCounter(metricsNames.SIGNIN_DEFRA_ID)
     return h.redirect(paths.SEARCH)
   }
 }

src/services/search-patterns.js
@@ -1,3 +1,5 @@
+import { metricsNames } from '../models/model-constants.js'
+
 export const searchKeys = {
   MRN: 'mrn',
   CHED_ID: 'chedId',
@@ -12,36 +14,43 @@ export const searchPatterns = [
   {
     key: searchKeys.MRN,
     pattern: /^\d{2}[A-Z]{2}[A-z0-9]{14}$/,
-    description: 'MRN'
+    description: 'MRN',
+    metricName: metricsNames.MRN
   },
   {
     key: searchKeys.CHED_ID,
     pattern: /^CHED([ADP]|P{2})\.GB\.2\d{3}\.\d{7}[VR]?$/,
-    description: 'CHED'
+    description: 'CHED',
+    metricName: metricsNames.CHED_ID
   },
   {
     key: searchKeys.CDS_CHED_ID,
     pattern: /^GBCHD2\d{3}\.\d{7}[VR]?$/,
-    description: 'CDS CHED'
+    description: 'CDS CHED',
+    metricName: metricsNames.CHED_ID
   },
   {
     key: searchKeys.PARTIAL_CHED,
     pattern: /^2\d{3}\.\d{7}[VR]?$/,
-    description: 'Partial CHED'
+    description: 'Partial CHED',
+    metricName: metricsNames.CHED_ID
   },
   {
     key: searchKeys.CHED_LAST_SEVEN_DIGITS,
     pattern: /^\d{7}[VR]?$/,
-    description: 'last 7 digits of a CHED'
+    description: 'last 7 digits of a CHED',
+    metricName: metricsNames.CHED_ID
   },
   {
     key: searchKeys.DUCR,
     pattern: /^\dGB\d{12}-(?:[0-9A-Z()-]{1,19})$/,
-    description: 'DUCR'
+    description: 'DUCR',
+    metricName: metricsNames.DUCR
   },
   {
     key: searchKeys.GMR_ID,
     pattern: /^GMR[A-Z][0-9A-Z]{8}$/,
-    description: 'GMR ID'
+    description: 'GMR ID',
+    metricName: metricsNames.GMR_ID
   }
 ]

test/unit/models/goods-vehicle-movements.test.js
@@ -1,4 +1,11 @@
 import { mapGoodsVehicleMovements } from '../../../src/models/goods-vehicle-movements'
+import { metricsNames } from '../../../src/models/model-constants.js'
+
+const mockMetricCounter = jest.fn()
+
+jest.mock('../../../src/utils/metrics.js', () => ({
+  metricsCounter: (...args) => mockMetricCounter(...args)
+}))
 
 test('GMR Vehicle details mapped', () => {
   const relatedImportDeclarationsPayload = {
@@ -538,4 +545,144 @@ test.each([
       }
     ]
   })
+})
+
+test.each([
+  {
+    transits: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      }
+    ],
+    expectedKnownMrnsCount: 1,
+    expectedUnknownMrnsCount: 0
+  },
+  {
+    transits: [
+      {
+        id: "25GB00000000000002"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      }
+    ],
+    expectedKnownMrnsCount: 2,
+    expectedUnknownMrnsCount: 0
+  },
+  {
+    transits: [],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      },
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 1,
+    expectedUnknownMrnsCount: 1
+  },
+  {
+    transits: [],
+    customs: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 0,
+    expectedUnknownMrnsCount: 1
+  },
+  {
+    transits: [
+      {
+        id: "25GB00000000000004"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 0,
+    expectedUnknownMrnsCount: 1
+  }
+])('Emits metrics when counts greater than zero and excludes unknown Transit MRNs', (options) => {
+  const relatedImportDeclarationsPayload = {
+    customsDeclarations: [
+      {
+        movementReferenceNumber: "25GB00000000000001",
+        clearanceDecision: {
+          items: [
+            {
+              checks: [
+                {
+                  checkCode: 'H224',
+                  decisionCode: 'E03'
+                }
+              ]
+            }
+          ],
+          results: [
+            {
+              checkCode: 'H224',
+              internalDecisionCode: 'E03'
+            }
+          ]
+        },
+        finalisation: null
+      },
+      {
+        movementReferenceNumber: "25GB00000000000002",
+        clearanceDecision: {
+          items: [
+            {
+              checks: [
+                {
+                  checkCode: 'H224',
+                  decisionCode: 'E03'
+                }
+              ]
+            }
+          ],
+          results: [
+            {
+              checkCode: 'H224',
+              internalDecisionCode: 'E03'
+            }
+          ]
+        },
+        finalisation: null
+      }
+    ],
+    goodsVehicleMovements: [
+      {
+        gmr: {
+          id: "GMRA00000AB1",
+          vehicleRegistrationNumber: "ABC 111",
+          trailerRegistrationNums: [ "ABC 222" ],
+          declarations: {
+            transits: options.transits,
+            customs: options.customs
+          }
+        }
+      }
+    ]
+  }
+
+  mapGoodsVehicleMovements(relatedImportDeclarationsPayload)
+
+  options.expectedKnownMrnsCount > 0
+    ? expect(mockMetricCounter).toHaveBeenCalledWith(metricsNames.GMR_KNOWN_MRNS, options.expectedKnownMrnsCount)
+    : expect(mockMetricCounter).not.toHaveBeenCalledWith(metricsNames.GMR_KNOWN_MRNS, 0)
+
+  options.expectedUnknownMrnsCount > 0
+    ? expect(mockMetricCounter).toHaveBeenCalledWith(metricsNames.GMR_UNKNOWN_MRNS, options.expectedUnknownMrnsCount)
+    : expect(mockMetricCounter).not.toHaveBeenCalledWith(metricsNames.GMR_UNKNOWN_MRNS, 0)
 })

test/unit/routes/gmr-search-result.test.js
@@ -1,0 +1,50 @@
+import { gmrSearchResult } from '../../../src/routes/gmr-search-result.js'
+import { metricsNames } from '../../../src/models/model-constants.js'
+
+const mockMetricCounter = jest.fn()
+const mockGetRelatedImportDeclarations = jest.fn()
+
+jest.mock('../../../src/utils/metrics.js', () => ({
+  metricsCounter: (...args) => mockMetricCounter(...args)
+}))
+
+jest.mock('../../../src/services/related-import-declarations.js', () => ({
+  getRelatedImportDeclarations: (...args) => mockGetRelatedImportDeclarations(...args)
+}))
+
+const mockRequest = {
+  query: {
+    searchTerm: 'GMRA00000000'
+  },
+  yar: {
+    flash: () => jest.fn()
+  }
+}
+
+const mockHandler = {
+  redirect: () => {
+    return {
+      takeover: jest.fn()
+    }
+  }
+}
+
+test('Should emit GMR search not found metric when no records found', async () => {
+  mockGetRelatedImportDeclarations.mockReturnValue({
+    goodsVehicleMovements: []
+  })
+
+  await gmrSearchResult.options.handler(mockRequest, mockHandler)
+
+  expect(mockMetricCounter).toHaveBeenCalledWith(metricsNames.GMR_NOT_FOUND)
+})
+
+test('Should emit GMR search metric', async () => {
+  mockGetRelatedImportDeclarations.mockReturnValue({
+    goodsVehicleMovements: []
+  })
+
+  await gmrSearchResult.options.pre[0].method(mockRequest, mockHandler)
+
+  expect(mockMetricCounter).toHaveBeenCalledWith(metricsNames.GMR_ID)
+})

DEFRA/btms-portal-frontend

src/models/model-constants.js
@@ -165,3 +165,15 @@ export const ORDERED_CLEARANCE_DECISIONS = [
   { type: 'item', code: 'C07', description: `Release - ${decisionCodeDescriptions.C07}` },
   { type: 'item', code: 'C08', description: `Release - ${decisionCodeDescriptions.C08}` }
 ]
+
+export const metricName = {
+  MRN: 'search.mrn',
+  CHED_ID: 'search.chedId',
+  DUCR: 'search.ducr',
+  GMR_ID: 'search.gmrId',
+  SIGNIN_ENTRA_ID: 'signIn.entraId',
+  SIGNIN_DEFRA_ID: 'signIn.defraId',
+  GMR_NOT_FOUND: 'gmr.search.not.found',
+  GMR_KNOWN_MRNS: 'gmr.known.mrns',
+  GMR_UNKNOWN_MRNS: 'gmr.unknown.mrns'
+}

src/routes/gmr-search-result.js
@@ -3,6 +3,8 @@ import { createRouteConfig } from './search-result-common.js'
 import { getRelatedImportDeclarations } from '../services/related-import-declarations.js'
 import { mapGoodsVehicleMovements } from '../models/goods-vehicle-movements.js'
 import { searchKeys } from '../services/search-patterns.js'
+import { metricsCounter } from '../utils/metrics.js'
+import { metricName } from '../models/model-constants.js'
 
 const searchTermValidator = (key, pattern, value) => {
   return key === searchKeys.GMR_ID && pattern.test(value)
@@ -18,6 +20,8 @@ export const gmrSearchResult = createRouteConfig(searchTermValidator, paths.GMR_
       isValid: false,
       errorCode: 'SEARCH_TERM_NOT_FOUND'
     })
+
+    metricsCounter(metricName.GMR_NOT_FOUND)
 
     return h.redirect(paths.SEARCH).takeover()
   }

src/routes/signin-oidc-entra.js
@@ -1,6 +1,7 @@
 import { paths } from './route-constants.js'
 import { setUserSession } from '../auth/user-session.js'
 import { metricsCounter } from '../utils/metrics.js'
+import { metricName } from '../models/model-constants.js'
 
 export const signinOidcEntra = {
   method: ['get', 'post'],
@@ -13,7 +14,7 @@ export const signinOidcEntra = {
     await setUserSession(request, sessionId)
     request.cookieAuth.set({ sessionId })
 
-    metricsCounter('signIn.entraId')
+    metricsCounter(metricName.SIGNIN_ENTRA_ID)
     return h.redirect(paths.SEARCH)
   }
 }

src/routes/signin-oidc.js
@@ -1,6 +1,7 @@
 import { paths } from './route-constants.js'
 import { setUserSession } from '../auth/user-session.js'
 import { metricsCounter } from '../utils/metrics.js'
+import { metricName } from '../models/model-constants.js'
 
 export const signinOidc = {
   method: ['get', 'post'],
@@ -13,7 +14,7 @@ export const signinOidc = {
     await setUserSession(request, sessionId)
     request.cookieAuth.set({ sessionId })
 
-    metricsCounter('signIn.defraId')
+    metricsCounter(metricName.SIGNIN_DEFRA_ID)
     return h.redirect(paths.SEARCH)
   }
 }

src/services/search-patterns.js
@@ -1,3 +1,5 @@
+import { metricName } from '../models/model-constants.js'
+
 export const searchKeys = {
   MRN: 'mrn',
   CHED_ID: 'chedId',
@@ -12,36 +14,43 @@ export const searchPatterns = [
   {
     key: searchKeys.MRN,
     pattern: /^\d{2}[A-Z]{2}[A-z0-9]{14}$/,
-    description: 'MRN'
+    description: 'MRN',
+    metricName: metricName.MRN
   },
   {
     key: searchKeys.CHED_ID,
     pattern: /^CHED([ADP]|P{2})\.GB\.2\d{3}\.\d{7}[VR]?$/,
-    description: 'CHED'
+    description: 'CHED',
+    metricName: metricName.CHED_ID
   },
   {
     key: searchKeys.CDS_CHED_ID,
     pattern: /^GBCHD2\d{3}\.\d{7}[VR]?$/,
-    description: 'CDS CHED'
+    description: 'CDS CHED',
+    metricName: metricName.CHED_ID
   },
   {
     key: searchKeys.PARTIAL_CHED,
     pattern: /^2\d{3}\.\d{7}[VR]?$/,
-    description: 'Partial CHED'
+    description: 'Partial CHED',
+    metricName: metricName.CHED_ID
   },
   {
     key: searchKeys.CHED_LAST_SEVEN_DIGITS,
     pattern: /^\d{7}[VR]?$/,
-    description: 'last 7 digits of a CHED'
+    description: 'last 7 digits of a CHED',
+    metricName: metricName.CHED_ID
   },
   {
     key: searchKeys.DUCR,
     pattern: /^\dGB\d{12}-(?:[0-9A-Z()-]{1,19})$/,
-    description: 'DUCR'
+    description: 'DUCR',
+    metricName: metricName.DUCR
   },
   {
     key: searchKeys.GMR_ID,
     pattern: /^GMR[A-Z][0-9A-Z]{8}$/,
-    description: 'GMR ID'
+    description: 'GMR ID',
+    metricName: metricName.GMR_ID
   }
 ]

test/unit/models/goods-vehicle-movements.test.js
@@ -1,4 +1,11 @@
 import { mapGoodsVehicleMovements } from '../../../src/models/goods-vehicle-movements'
+import { metricName } from '../../../src/models/model-constants.js'
+
+const mockMetricCounter = jest.fn()
+
+jest.mock('../../../src/utils/metrics.js', () => ({
+  metricsCounter: (...args) => mockMetricCounter(...args)
+}))
 
 test('GMR Vehicle details mapped', () => {
   const relatedImportDeclarationsPayload = {
@@ -538,4 +545,144 @@ test.each([
       }
     ]
   })
+})
+
+test.each([
+  {
+    transits: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      }
+    ],
+    expectedKnownMrnsCount: 1,
+    expectedUnknownMrnsCount: 0
+  },
+  {
+    transits: [
+      {
+        id: "25GB00000000000002"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      }
+    ],
+    expectedKnownMrnsCount: 2,
+    expectedUnknownMrnsCount: 0
+  },
+  {
+    transits: [],
+    customs: [
+      {
+        id: "25GB00000000000001"
+      },
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 1,
+    expectedUnknownMrnsCount: 1
+  },
+  {
+    transits: [],
+    customs: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 0,
+    expectedUnknownMrnsCount: 1
+  },
+  {
+    transits: [
+      {
+        id: "25GB00000000000004"
+      }
+    ],
+    customs: [
+      {
+        id: "25GB00000000000003"
+      }
+    ],
+    expectedKnownMrnsCount: 0,
+    expectedUnknownMrnsCount: 1
+  }
+])('Emits metrics when counts greater than zero and excludes unknown Transit MRNs', (options) => {
+  const relatedImportDeclarationsPayload = {
+    customsDeclarations: [
+      {
+        movementReferenceNumber: "25GB00000000000001",
+        clearanceDecision: {
+          items: [
+            {
+              checks: [
+                {
+                  checkCode: 'H224',
+                  decisionCode: 'E03'
+                }
+              ]
+            }
+          ],
+          results: [
+            {
+              checkCode: 'H224',
+              internalDecisionCode: 'E03'
+            }
+          ]
+        },
+        finalisation: null
+      },
+      {
+        movementReferenceNumber: "25GB00000000000002",
+        clearanceDecision: {
+          items: [
+            {
+              checks: [
+                {
+                  checkCode: 'H224',
+                  decisionCode: 'E03'
+                }
+              ]
+            }
+          ],
+          results: [
+            {
+              checkCode: 'H224',
+              internalDecisionCode: 'E03'
+            }
+          ]
+        },
+        finalisation: null
+      }
+    ],
+    goodsVehicleMovements: [
+      {
+        gmr: {
+          id: "GMRA00000AB1",
+          vehicleRegistrationNumber: "ABC 111",
+          trailerRegistrationNums: [ "ABC 222" ],
+          declarations: {
+            transits: options.transits,
+            customs: options.customs
+          }
+        }
+      }
+    ]
+  }
+
+  mapGoodsVehicleMovements(relatedImportDeclarationsPayload)
+
+  options.expectedKnownMrnsCount > 0
+    ? expect(mockMetricCounter).toHaveBeenCalledWith(metricName.GMR_KNOWN_MRNS, options.expectedKnownMrnsCount)
+    : expect(mockMetricCounter).not.toHaveBeenCalledWith(metricName.GMR_KNOWN_MRNS, 0)
+
+  options.expectedUnknownMrnsCount > 0
+    ? expect(mockMetricCounter).toHaveBeenCalledWith(metricName.GMR_UNKNOWN_MRNS, options.expectedUnknownMrnsCount)
+    : expect(mockMetricCounter).not.toHaveBeenCalledWith(metricName.GMR_UNKNOWN_MRNS, 0)
 })

test/unit/routes/gmr-search-result.test.js
@@ -1,0 +1,50 @@
+import { gmrSearchResult } from '../../../src/routes/gmr-search-result.js'
+import { metricName } from '../../../src/models/model-constants.js'
+
+const mockMetricCounter = jest.fn()
+const mockGetRelatedImportDeclarations = jest.fn()
+
+jest.mock('../../../src/utils/metrics.js', () => ({
+  metricsCounter: (...args) => mockMetricCounter(...args)
+}))
+
+jest.mock('../../../src/services/related-import-declarations.js', () => ({
+  getRelatedImportDeclarations: (...args) => mockGetRelatedImportDeclarations(...args)
+}))
+
+const mockRequest = {
+  query: {
+    searchTerm: 'GMRA00000000'
+  },
+  yar: {
+    flash: () => jest.fn()
+  }
+}
+
+const mockHandler = {
+  redirect: () => {
+    return {
+      takeover: jest.fn()
+    }
+  }
+}
+
+test('Should emit GMR search not found metric when no records found', async () => {
+  mockGetRelatedImportDeclarations.mockReturnValue({
+    goodsVehicleMovements: []
+  })
+
+  await gmrSearchResult.options.handler(mockRequest, mockHandler)
+
+  expect(mockMetricCounter).toHaveBeenCalledWith(metricName.GMR_NOT_FOUND)
+})
+
+test('Should emit GMR search metric', async () => {
+  mockGetRelatedImportDeclarations.mockReturnValue({
+    goodsVehicleMovements: []
+  })
+
+  await gmrSearchResult.options.pre[0].method(mockRequest, mockHandler)
+
+  expect(mockMetricCounter).toHaveBeenCalledWith(metricName.GMR_ID)
+})