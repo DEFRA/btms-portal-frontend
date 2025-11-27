8957304e1ef9d447fdbe74c908dcb5f684ce4a75
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-11-27 14:45:16 +0000

    CDMS-626: sonar fix

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -3,8 +3,8 @@ export const sortDescendingAndSearchTermMatchAlwaysFirst = (searchTerm, searchTe
     const aMatches = searchTermMatcher(searchTerm, a)
     const bMatches = searchTermMatcher(searchTerm, b)
 
-    if (aMatches && !bMatches) return -1
-    if (bMatches && !aMatches) return 1
+    if (aMatches && !bMatches) { return -1 }
+    if (bMatches && !aMatches) { return 1 }
 
     const aUpdated = new Date(a.updated).getTime()
     const bUpdated = new Date(b.updated).getTime()
