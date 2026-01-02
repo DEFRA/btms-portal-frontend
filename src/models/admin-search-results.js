f50287c2302f03281f1ccfc2c77b957e65ad5fbd
tal-nagra <62334836+tal-nagra@users.noreply.github.com>
2026-01-02 14:12:51 +0000

    Remove the use of a JSON.stringify replacer function

--- src/models/admin-search-results.js
+++ src/models/admin-search-results.js
@@ -4,34 +4,31 @@ const searchTypesNeedingMsgSerialisation = new Set([
   ADMIN_SEARCH_TYPES.ALL_EVENTS
 ])
 
-const prettyPrint = (obj, adminSearchType) => {
-  const serialisedObj = JSON.stringify(
-    obj,
-    (key, value) => {
-      if (adminSearchType === ADMIN_SEARCH_TYPES.ALL_EVENTS
-        && key === 'changeSet') {
-        // exclude the changeSet field
-        return undefined
-      }
-      if (searchTypesNeedingMsgSerialisation.has(adminSearchType)
-        && key === 'message') {
-        // deserialise the message field
-        return JSON.parse(value)
-      }
-      return value
-    },
-    2)
-  return serialisedObj
+const prettyPrint = (obj) => {
+  // A 'replacer' function has intentionally not
+  // been provided to JSON.stringify here
+  // as it isn't straightforward to determine the context for
+  // the "message" and "changeSet" fields in order to replace their values
+  return JSON.stringify(obj, null, 2)
 }
 
 export const mapAdminSearchResults = (rawSearchResults, searchType) => {
   switch (searchType) {
     case ADMIN_SEARCH_TYPES.ALL_MESSAGES:
+      return rawSearchResults
+        .map(rawMsg => {
+          rawMsg.message = JSON.parse(rawMsg.message)
+          return prettyPrint(rawMsg)
+        })
     case ADMIN_SEARCH_TYPES.ALL_EVENTS:
-      return rawSearchResults.map(r => prettyPrint(r, searchType))
-    case ADMIN_SEARCH_TYPES.INFORMATION: {
-      return prettyPrint(rawSearchResults, searchType)
-    }
+      return rawSearchResults
+        .map(resourceEvent => {
+          resourceEvent.message = resourceEvent.message.replace(/,\s*"changeSet":\[.*]/, '')
+          resourceEvent.message = JSON.parse(resourceEvent.message)
+          return prettyPrint(resourceEvent)
+        })
+    case ADMIN_SEARCH_TYPES.INFORMATION:
+      return prettyPrint(rawSearchResults)
     default:
       throw new Error(`Unsupported admin search type: ${searchType}`)
   }
